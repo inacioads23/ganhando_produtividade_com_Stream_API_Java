8edf5f12b4dca69ea2a2d18dc440604226dd485f
inacioads23 <jinacio11@gmail.com>
2025-07-02 15:42:52 -0300

    Updated Functional Interface examples - Consumer

--- src/functional_interface/examples/ConsumerExample1.java
+++ src/functional_interface/examples/ConsumerExample1.java
@@ -1,5 +1,8 @@
 package functional_interface.examples;
 
+// Consumer<T>: Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
+// É utilizada principalmente para realizar ações ou efeitos colaterais nos elementos do Stream sem modificar ou retornar um valor.
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
