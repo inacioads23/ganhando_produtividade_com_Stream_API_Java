ca0bd43088e8e09949fa667ae53b788d321e4a66
inacioads23 <jinacio11@gmail.com>
2025-07-02 15:38:37 -0300

    Functional Interface examples - Supplier

---
+++ src/functional_interface/examples/SupplierExample1.java
@@ -0,0 +1,28 @@
+package functional_interface.examples;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SupplierExample1 {
+	
+	public static void main(String[] args) {
+		// Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+		Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+		
+		// Usar o Supplier para obter uma lista com 5 saudações		
+		List<String> listaSaudacoes = Stream.generate(saudacao).limit(5).collect(Collectors.toList());
+		List<String> listaSaudacoes1 = Stream.generate(saudacao).limit(5).toList();
+	
+		
+		// Imprimir as saudações geradas
+		System.out.println("Opção1 - Usando Method Reference");
+		listaSaudacoes.forEach(System.out::println);
+		
+		
+		System.out.println("\nOpção2 - Usando Lambda");
+		listaSaudacoes1.forEach(s -> System.out.println(s));
+	}
+
+}
